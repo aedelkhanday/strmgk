c67faf4d3cae54bb91191807283a30671f6b9512
Adil <fabaadil007@gmail.com>
2019-09-04 09:48:41 +0530

    Added a method for identifying if a substring is present in a specific string

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = {
   trimSpaces: function(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    if (typeof string !== "string")
+      throw new TypeError("Please provide a string!");
     return string.replace(/\s/g, "");
+  },
+  isSubstringPresent: function(string, subString) {
+    if (string.indexOf(subString) > -1) return true;
+    else return false;
   }
 };
